178239e6bf58c3ba1078e44a108f7076ec3cbceb
tli-gini <ginitong.fish0524@gmail.com>
2024-09-18 23:46:21 +0800

    google sign in & RWD

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"], // Add Firebase storage domain
+    domains: [
+      "firebasestorage.googleapis.com", // Firebase storage domain
+      "lh3.googleusercontent.com", // Google user profile images domain
+    ],
   },
 };
 
